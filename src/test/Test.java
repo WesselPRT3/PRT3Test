a1c9e87e10c211bb44a777dbf6ab4096db846691
s1673068 <s1673068@u036238.vuw.leidenuniv.nl>
2016-04-19 16:33:20 +0200

    Remove hoi

---
+++ src/test/Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author s1673068
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {        
+        Foo foo = new Foo(2,3);
+        Foo.Bar bar = foo.new Bar(4,1);
+        
+        bar.print();
+        
+    }
+    
+}
